9d420583b5dcca2fdba7e0302927c56b5ff74555
Lucas Corrêa <65604927+correa0105@users.noreply.github.com>
2022-07-04 12:38:22 -0300

    Limpando comentários excessivos

--- assets/js/taskList.js
+++ assets/js/taskList.js
@@ -1,78 +1,78 @@
-const inputNewTask = document.querySelector(".input-newTask");      //SELECIONA A CLASSE
-const btnAddTask = document.querySelector(".btn-AddTask");          //SELECIONA A CLASSE
-const tasks = document.querySelector(".tasks");                     //SELECIONA A CLASSE
+const inputNewTask = document.querySelector(".input-newTask");
+const btnAddTask = document.querySelector(".btn-AddTask");
+const tasks = document.querySelector(".tasks");
 
-btnAddTask.addEventListener("click", function() {                   //CAPTURA EVENTO DE CLICK DO BOTAO
-    if (!inputNewTask.value) return                                 //SE A CLASSE FOR DIFERENTE DE VAZIO RETORNA A
-    createTask(inputNewTask.value);                                 //FUNÇÃO DE CRIAR TAREFA COM O VALOR PREENCHIDO
+btnAddTask.addEventListener("click", function() {
+    if (!inputNewTask.value) return
+    createTask(inputNewTask.value);
 });
 
-inputNewTask.addEventListener("keypress", function(event) {         //MESMA FUNÇÃO ANTERIOR POREM COM A OPÇÃO DE DENTER
+inputNewTask.addEventListener("keypress", function(event) {
     if (event.keyCode === 13) {                     
         if (!inputNewTask.value) return
         createTask(inputNewTask.value);
     }
 });
 
-document.addEventListener("click", function(event) {                //CAPTURA EVENTO DE CLICK DE TODO O DOCUMENT
-    const element = event.target;                                   //CRIA A VARIAVEL E ARMAZENA O LUGAR CLICADO
+document.addEventListener("click", function(event) {
+    const element = event.target;
 
-    if (element.classList.contains("removeTask")) {                 //SE O ELEMENTO CONTEM A CLASSE REMOVETASK 
-        if (confirm("Sera o excluido o item " + (element.parentElement.innerText.replace("Apagar","")))){ //USA A FUNÇÃO CONFIRM PARA EXCLUIR O ITEM               
-        element.parentElement.remove();                             //REMOVE TODOS OS PARENTES;
-        saveTasks();}                                               //CHAMA A CLASSE SALVARTASKS
+    if (element.classList.contains("removeTask")) {
+        if (confirm("Sera o excluido o item " + (element.parentElement.innerText.replace("Apagar","")))){              
+        element.parentElement.remove();
+        saveTasks();}
     }
 
 })
 
-function createLi() {                                               //CRIA UM ITEM NA LISTA (UM ELEMENTO LI)
+function createLi() {
     const li = document.createElement("li");
     return li;                      
 }
 
-function createTask(textInput) {                                    //CRIA UMA TAREFA
-    const li = createLi();                                          //ARMAZENA A FUNÇÃO DE CRIAR LI EM UAM CONSTANTE
+function createTask(textInput) {
+    const li = createLi();
     li.innerText = textInput;                                       
-    tasks.appendChild(li);                                          //A CLASSE TASKS RECEBE O LI CRIADO JUNTAMENTE COM O TEXTO
-    cleanInput();                                                   //LIMPA O INPUT
-    createButtonRemove(li);                                         //CRIA O BOTAO REMOVER
-    saveTasks();                                                    //CHAMA A CLASSE DE SALVAR AS TASKS
+    tasks.appendChild(li);
+    cleanInput();
+    createButtonRemove(li);
+    saveTasks();
 }
 
-function cleanInput() {                                             //LIMPA O INPUT
-    inputNewTask.value = "";                                        //DETERMIAN O INPUT VAZIO
-    inputNewTask.focus();                                           //DA FOCUS NO INPUT
+function cleanInput() {
+    inputNewTask.value = "";
+    inputNewTask.focus();
 }
 
-function createButtonRemove(li) {                                   //CRIA BOTAO REMOVER
-    const btnRemove = document.createElement("button");             //ARMAZENA A CRIAÇÃO DE UM BOTÃO EM UMA CONSTANTE
-    btnRemove.setAttribute("class", "removeTask");                  //O BOTAO RECEBE A CLASS REMOVETASK
-    btnRemove.innerText = "Apagar";                                 //BOTAO REMOVE RECEBE O TEXTO APAGAR
+function createButtonRemove(li) {
+    const btnRemove = document.createElement("button");
+    btnRemove.setAttribute("class", "removeTask");
+    btnRemove.innerText = "Apagar";
 
-    li.appendChild(btnRemove);                                      //O LI RECEBE O BOTAO REMOVE
+    li.appendChild(btnRemove);
 }
 
-function saveTasks() {                                              //FUNÇÃO PARA SALVAR AS TASKS
-    const liTasks = tasks.querySelectorAll("li");                   //SELECIONA TODOS ELEMENTOS LI DENTRO DE TASKS
-    const arrayListTasks = [];                                      //CRIA O ARRAY DAS TAREFAS
+function saveTasks() {
+    const liTasks = tasks.querySelectorAll("li");
+    const arrayListTasks = [];
 
-    for (let task of liTasks) {                                     //CRIA UMA VARIAVEL LET QUE VAI ARMAZENANDO O LI
-        let taskText = task.innerText;                              //ARMAZENA O INNER TEXT DO LI EM UMA VARIAVEL CHAMADA TASKTEXT
-        taskText = taskText.replace("Apagar", "").trim();           //TASK TEXT RECEBE A SUBSTITUIÇÃO DE APAGAR POR VAZIO
-        arrayListTasks.push(taskText);                              //ADICIONA AO FINAL DO ARRAY LIST O TEXTO ARMAZENADO PELA VARIAVEL TEXT
+    for (let task of liTasks) {
+        let taskText = task.innerText;
+        taskText = taskText.replace("Apagar", "").trim();
+        arrayListTasks.push(taskText);
     }
 
-    const taskJSON = JSON.stringify(arrayListTasks);                //ARMAZENA EM UMA CONSTANTE A CONVERSÃO DO ARRAY LIST PARA JSON STRING
-    localStorage.setItem("tasksSaved", taskJSON);                   //LOCAL STORAGE DO NAVEGADOR RECEBE A FUNÇÃO ARMAZENADA NA TASKJSON
+    const taskJSON = JSON.stringify(arrayListTasks);
+    localStorage.setItem("tasksSaved", taskJSON);
 }
 
-function addTasksSaved() {                                          //ATUALIZA A PAGINA COM AS TASKS SALVAS
-    const tasks = localStorage.getItem("tasksSaved");               //ARMAZENA OS DADOS DO LOCAL STORAGE EM UMA CONSTANTE
-    const tasksList = JSON.parse(tasks);                            //TRANSFORMA OS DADOS DO STORAGE EM ARRAY EM ARMAZENA EM UMA VARIAVEL
+function addTasksSaved() {
+    const tasks = localStorage.getItem("tasksSaved");
+    const tasksList = JSON.parse(tasks);
 
-    for (let task of tasksList) {                                   //CRIA UMA VARIAVEL TASK QUE VAI ARMAZENANDO O CADA ITEM DO ARRAY
-        createTask(task);                                           //CHAMA A FUNÇÃO CREATETASK COM O VALOR DA VARIAVEL TASK
+    for (let task of tasksList) {
+        createTask(task);
     }
 }
 
-addTasksSaved();                                                    //SEMPRE CHAMA A FUNÇÃO DE ATUALZIAR A APGINA COM AS TASKS SALVAS
+addTasksSaved();
